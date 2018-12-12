62d1d8f263f5a6e5556bf2e590eb960dc1fe01c8
oleg laban <oleg19432@gmail.com>
2018-12-12 14:59:09 +0300

    Add comments in file routes.php

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,6 @@
 <?php
 return array(
+    //Пути ЧПУ 
     
     'product/([0-9]+)' => 'product/view/$1', //  controller 'product' action 'view'
     
@@ -11,24 +12,25 @@ return array(
     
     'contact' => 'site/contact',
     
+    //Пути для контроллера корзины 
     'cart/addAjax/([0-9]+)'=> 'cart/addAjax/$1',
     'cart/add' => 'cart/add',
     'cart/checkout' => 'cart/checkout',
     'cart' => 'cart/index',
     
-    //Пути для категорий
+    //Пути для категорий (админ панель)
     'admin/category/delete/([0-9]+)' => 'adminCategory/delete/$1',
     'admin/category/update/([0-9]+)' => 'adminCategory/update/$1',
     'admin/category/create' => 'adminCategory/create',
     'admin/category' => 'adminCategory/index',
     
-    //Пути для заказов
+    //Пути для заказов (админ панель)
     'admin/order/delete/([0-9]+)' => 'adminOrder/delete/$1',
     'admin/order/update/([0-9]+)' => 'adminOrder/update/$1',
     'admin/order/view/([0-9]+)' => 'adminOrder/view/$1',
     'admin/order' => 'adminOrder/index',
     
-    //Пути для продуктов
+    //Пути для продуктов (админ панель)
     'admin/product/delete/([0-9]+)' => 'adminProduct/delete/$1',
     'admin/product/update/([0-9]+)' => 'adminProduct/update/$1',
     'admin/product/create' => 'adminProduct/create',
@@ -37,10 +39,12 @@ return array(
     
     'admin' => 'admin/index',
     
+    //Пути для контроллера регистрации
     'user/register' => 'user/register',
     'user/login' => 'user/login',
     'user/logout' => 'user/logout',
     
+    //пути для кабинета
     'cabinet/edit' => 'cabinet/edit',
     'cabinet' => 'cabinet/index',
     
